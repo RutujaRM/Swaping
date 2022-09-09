c39c3f3aa7dcd0be635a1051e122773f8da43d0d
Rutuja R Mashere <96306705+RutujaRM@users.noreply.github.com>
2022-09-09 11:57:52 -0600

    Update Swaping.js

--- src/Components/Swaping.js
+++ src/Components/Swaping.js
@@ -11,13 +11,35 @@ const SwapComponent = () => {
     setA(bTemp); // swap a
     setB(aTemp); // swap b
   };
+  const style = {
+    backgroundColor: "skyblue",
+    color: "white",
+    height: "3rem",
+    textAlign: "center"
+  };
+  const style1 = {
+    backgroundColor: "crimson",
+    color: "white",
+    height: "3rem",
+    textAlign: "center"
+  };
+  const btn = {
+    backgroundColor: "purple",
+    color: "white",
+    height: "3rem",
+    width: "10rem",
+    borderRadius: "0.5rem",
+    fontWeight: "14px"
+  };
 
   return (
     <>
-      <h3>A :- {a} </h3>
-      <h3>B :- {b} </h3>
+      <h3 style={style}>A :- {a} </h3>
+      <h3 style={style1}>B :- {b} </h3>
 
-      <button onClick={onClick}>Swap</button>
+      <button onClick={onClick} style={btn}>
+        Swap
+      </button>
     </>
   );
 };
